cb69de823c0e3fa13a84d4bcda8b81aa2d962004
Painketsu <mypainisname@gmail.com>
2020-08-31 05:13:00 +0200

    Add project files.

---
+++ RegistryRunRemove/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryRunRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1)
+                    Remove(args);
+                else
+                    Console.WriteLine("1 param, command should be [remove Name]");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private static void Remove(string[] args)
+        {
+            string name = args[0] + ".exe";
+            string KeyName = string.Format(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\{0}", name);
+
+            RegistryKey reg = null;
+            reg = Registry.LocalMachine.OpenSubKey(KeyName, RegistryKeyPermissionCheck.ReadWriteSubTree);
+            if (reg != null)
+            {
+                reg.Close();
+                Registry.LocalMachine.DeleteSubKey(KeyName);
+            }
+
+        }
+    }
+}
